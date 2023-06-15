17f93f5027345ef10cbaeda4e7ce238b86e40601
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-06-15 23:45:23 +0530

    Add variants

--- frontend/src/components/atoms/Button/index.tsx
+++ frontend/src/components/atoms/Button/index.tsx
@@ -12,27 +12,79 @@ interface ButtonProps {
   title: string;
   style?: ViewStyle;
   textStyle?: TextStyle;
+  variation?: 'primary' | 'secondary' | 'outlined';
 }
 
-const Button: FC<ButtonProps> = ({onPress, title, style, textStyle}) => {
+const Button: FC<ButtonProps> = ({
+  onPress,
+  title,
+  style,
+  textStyle,
+  variation = 'primary',
+}) => {
+  const getButtonStyles = () => {
+    switch (variation) {
+      case 'primary':
+        return [styles.button, styles.primaryButton, style];
+      case 'secondary':
+        return [styles.button, styles.secondaryButton, style];
+      case 'outlined':
+        return [styles.button, styles.outlinedButton, style];
+      default:
+        return [styles.button, style];
+    }
+  };
+
+  const getButtonTextStyles = () => {
+    switch (variation) {
+      case 'primary':
+        return [styles.buttonText, styles.primaryButtonText, textStyle];
+      case 'secondary':
+        return [styles.buttonText, styles.secondaryButtonText, textStyle];
+      case 'outlined':
+        return [styles.buttonText, styles.outlinedButtonText, textStyle];
+      default:
+        return [styles.buttonText, textStyle];
+    }
+  };
+
   return (
-    <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
-      <Text style={[styles.buttonText, textStyle]}>{title}</Text>
+    <TouchableOpacity style={getButtonStyles()} onPress={onPress}>
+      <Text style={getButtonTextStyles()}>{title}</Text>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: 'blue',
     padding: 10,
     borderRadius: 5,
     alignItems: 'center',
+  },
+  primaryButton: {
+    backgroundColor: 'blue',
+  },
+  secondaryButton: {
+    backgroundColor: 'black',
+  },
+  outlinedButton: {
+    borderWidth: 1,
+    borderColor: 'blue',
+    backgroundColor: 'transparent',
   },
   buttonText: {
     color: 'white',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  primaryButtonText: {
+    color: 'white',
+  },
+  secondaryButtonText: {
+    color: 'white',
+  },
+  outlinedButtonText: {
+    color: 'blue',
   },
 });
 
