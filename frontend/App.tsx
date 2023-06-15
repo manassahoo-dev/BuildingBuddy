55ec91a9010e2de5f4fd6f2b78cf1284c07aaba1
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-06-15 23:45:30 +0530

    Update App

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,80 +3,97 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import type {PropsWithChildren} from 'react';
 import React from 'react';
-import {Button, StyleSheet, Text, useColorScheme, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, useColorScheme, View} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Button from './src/components/atoms/Button';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
 const UserItem = ({user}) => {
+  const handleButtonPress = () => {};
   return (
     <View style={styles.container}>
       <Text style={styles.flatNumber}>{user.flatNumber}</Text>
-      <Text style={styles.username}>{user.username}</Text>
+      <Text style={styles.username}>{user.fullName}</Text>
       <Text>{user.mobile}</Text>
+      <Button
+        onPress={handleButtonPress}
+        title="Primary Button"
+        variation="primary"
+      />
+      <Button
+        onPress={handleButtonPress}
+        title="Secondary Button"
+        variation="secondary"
+      />
+      <Button
+        onPress={handleButtonPress}
+        title="Outlined Button"
+        variation="outlined"
+      />
     </View>
   );
 };
 
 const HomeScreen = ({navigation}) => {
   const users = [
-    {flatNumber: '101', username: 'Radha Krishna', mobile: 1510936378},
+    {flatNumber: '101', fullName: 'Radha Krishna', mobile: 1510936378},
     {
       flatNumber: '102',
-      username: 'KuppuRaj Chinna Swamy',
+      fullName: 'KuppuRaj Chinna Swamy',
       mobile: 8884141141,
     },
-    {flatNumber: '103', username: 'Nagesh', mobile: 9550664454},
-    {flatNumber: '104', username: 'Vishnu', mobile: 9739142699},
+    {flatNumber: '103', fullName: 'Nagesh', mobile: 9550664454},
+    {flatNumber: '104', fullName: 'Vishnu', mobile: 9739142699},
 
     {
       id: 3,
       flatNumber: '201',
-      username: 'Pesala Prasad Reddy',
+      fullName: 'Pesala Prasad Reddy',
       mobile: 9949026877,
     },
-    {flatNumber: '202', username: 'Manas Sahoo', mobile: 8523874754},
-    {flatNumber: '203', username: 'Ashish Gupta', mobile: 9999986897},
-    {flatNumber: '204', username: 'Chiranjeevi', mobile: 9542472200},
+    {flatNumber: '202', fullName: 'Manas Sahoo', mobile: 8523874754},
+    {flatNumber: '203', fullName: 'Ashish Gupta', mobile: 9999986897},
+    {flatNumber: '204', fullName: 'Chiranjeevi', mobile: 9542472200},
 
-    {flatNumber: '301', username: 'Ramana', mobile: 8971576789},
-    {flatNumber: '302', username: 'Manoj', mobile: 9032401648},
-    {flatNumber: '303', username: 'Vivek', mobile: 9650690169},
-    {flatNumber: '304', username: 'Dilleep', mobile: 7846914999},
+    {flatNumber: '301', fullName: 'Ramana', mobile: 8971576789},
+    {flatNumber: '302', fullName: 'Manoj', mobile: 9032401648},
+    {flatNumber: '303', fullName: 'Vivek', mobile: 9650690169},
+    {flatNumber: '304', fullName: 'Dilleep', mobile: 7846914999},
 
     {
       id: 3,
       flatNumber: '401',
-      username: 'Harish Kathuroju',
+      fullName: 'Harish Kathuroju',
       mobile: 9010445500,
     },
-    {flatNumber: '402', username: 'Naresh', mobile: 8019141341},
+    {flatNumber: '402', fullName: 'Naresh', mobile: 8019141341},
     {
       id: 3,
       flatNumber: '403',
-      username: 'Venkateshwara  Reddy',
+      fullName: 'Venkateshwara  Reddy',
       mobile: 9949026878,
     },
-    {flatNumber: '404', username: 'Rajesh', mobile: 7406222003},
+    {flatNumber: '404', fullName: 'Rajesh', mobile: 7406222003},
 
-    {flatNumber: '501', username: 'Kishan', mobile: 9676825800},
-    {flatNumber: '502', username: 'Mahesh Narayan', mobile: 9676126147},
-    {flatNumber: '503', username: 'Narashima Reddy', mobile: 9948121323},
-    {flatNumber: '504', username: 'Sampath', mobile: 9642052023},
+    {flatNumber: '501', fullName: 'Kishan', mobile: 9676825800},
+    {flatNumber: '502', fullName: 'Mahesh Narayan', mobile: 9676126147},
+    {flatNumber: '503', fullName: 'Narashima Reddy', mobile: 9948121323},
+    {flatNumber: '504', fullName: 'Sampath', mobile: 9642052023},
 
     // Add more user data as needed
   ];
 
   return (
-    <View>
+    <ScrollView>
       {users.map(user => (
         <UserItem key={user.mobile} user={user} />
       ))}
-    </View>
+    </ScrollView>
   );
 };
 const ProfileScreen = ({navigation, route}) => {
